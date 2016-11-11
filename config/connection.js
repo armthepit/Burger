59c3736ac60cd8ec21c4182fa728583195f22bdf
John Nowinsky <armthepit@armthepit.com>
2016-11-11 08:32:46 -0600

    add Heroku mysql connection parameters

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,26 @@
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'homework',
-	password: 'homework',
-	database: 'burgers_db'
-});
+var connection;
 
-connection.connect(function (error) {
-	if (error) {
-		console.error('error connecting: ' + error.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadId);
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'homework',
+        password: 'homework',
+        database: 'burgers_db'
+    });
+}
+
+connection.connect(function(error) {
+    if (error) {
+        console.error('error connecting: ' + error.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
